17701737f3b59f93a94aab45b827ab558e49e1bd
AkashSasank <akashsasank369@gmail.com>
2019-09-27 17:01:31 +0530

    added js for loading index pages

---
+++ js/signedup.js
@@ -0,0 +1,8 @@
+$(()=>{
+    $("#header").load("./components/header/header.html");
+    $("#hero").load("./components/home/hero/hero2.html");
+    $("#body").load("./components/home/home.html");
+    $("#signup").load("./components/home/signup/signup2.html");
+    $("#hash").load("./components/hash/hash.html");
+    $("#footer").load("./components/footer/footer.html");
+});

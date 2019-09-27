8466b180b3b3e3b816194fef26bb19587777fdf7
AkashSasank <akashsasank369@gmail.com>
2019-09-27 16:43:18 +0530

    js changed

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(()=>{
     $("#header").load("./components/header/header.html");
     $("#hero").load("./components/home/hero/hero1.html");
     $("#body").load("./components/home/home.html");
-    $("#signup").load("./components/signup/signup2.html");
+    $("#signup").load("./components/home/signup/signup1.html");
     $("#hash").load("./components/hash/hash.html");
     $("#footer").load("./components/footer/footer.html");
 });

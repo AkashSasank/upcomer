d28d7d4da6b5105eb3abb8c5726f6cade557561e
AkashSasank <akashsasank369@gmail.com>
2019-09-27 15:27:20 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(()=>{
+    $("#header").load("./components/header/header.html");
+    $("#hero").load("./components/home/hero/hero1.html");
+    $("#body").load("./components/home/home.html");
+    $("#signup").load("./components/signup/signup1.html");
+    $("#hash").load("./components/hash/hash.html");
+    $("#footer").load("./components/footer/footer.html");
+});
